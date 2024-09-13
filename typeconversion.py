1347ba2b0d8cb975de67a2b8e00c7132cf757822
AAYUSH ANAND SINHA <98117714+Aayushsinha09@users.noreply.github.com>
2024-09-13 19:29:03 +0530

    Add files via upload

---
+++ typeconversion.py
@@ -0,0 +1,20 @@
+x=14  #int
+y=2.69 #float
+z=1j  #complex
+
+#convert from int to float:
+a= float(x)
+
+#convert from float to int:
+b= int(y)
+
+#convert from int to complex:
+c= complex(z)
+
+print(a)
+print(b)
+print(c)
+
+print(type(x))
+print(type(y))
+print(type(z))
